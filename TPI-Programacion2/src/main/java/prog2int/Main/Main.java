bbb574cfc677d9ab649ff183e87fc2ed552aa3c6
nicolas <nicolas.viruel@phinxlab.com>
2025-11-06 12:38:35 -0300

    Se agregan los modelos, DAOS, Servicios y AppMenuDisplay

--- TPI-Programacion2/src/main/java/prog2int/Main/Main.java
+++ TPI-Programacion2/src/main/java/prog2int/Main/Main.java
@@ -2,6 +2,7 @@ package prog2int.Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        AppMenu app = new AppMenu();
+        app.run();
     }
 }
